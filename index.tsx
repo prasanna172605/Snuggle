7c74d246223b0d13968cf6be8665928d3aea3bd3
Prasanna Iyappan <named.as.prasanna@gmail.com>
2026-01-08 10:16:04 +0530

    Fix: Register Service Worker for Push Notifications

--- index.tsx
+++ index.tsx
@@ -15,3 +15,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((err) => {
+      console.log('Service Worker registration failed:', err);
+    });
+}
